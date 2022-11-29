9b4b5c0135750e4df15382f78a6c368422a5d692
Dieter Luypaert <hey@moeriki.com>
2022-11-29 09:15:59 +0100

    test: basic quality / sellIn degradation

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,22 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+  it ('should degrade sellIn', () => {
+    let shop = new Shop([new Item("foo", 1, 0)]);
+    shop = new Shop( shop.updateQuality());
+    expect(shop.items[0].sellIn).toBe(0);
+  })
+
+  it ('should degrade quality by one when not expired', () => {
+    let shop = new Shop([new Item("foo", 1, 1)]);
+    shop = new Shop(shop.updateQuality())
+    expect(shop.items[0].quality).toBe(0);
+  })
+
+  it('should degrade quality by two when expired', () => {
+    let shop = new Shop([new Item("foo", 1, 10)]);
+    shop = new Shop(shop.updateQuality());
+    shop = new Shop(shop.updateQuality());
+    expect(shop.items[0].quality).toBe(7);
+  })
 });
