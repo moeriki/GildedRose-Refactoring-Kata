5f44e1c153414bea70e20c68976e1d4bb4ff4b4f
Dieter Luypaert <hey@moeriki.com>
2022-11-29 09:17:55 +0100

    format: add prettier

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,22 +1,22 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it ('should degrade sellIn', () => {
-    let shop = new Shop([new Item("foo", 1, 0)]);
+describe('Gilded Rose', function () {
+  it('should degrade sellIn', () => {
+    let shop = new Shop([new Item('foo', 1, 0)]);
     shop = shop.updateQuality();
     expect(shop.items[0].sellIn).toBe(0);
-  })
+  });
 
-  it ('should degrade quality by one when not expired', () => {
-    let shop = new Shop([new Item("foo", 1, 1)]);
-    shop = shop.updateQuality()
+  it('should degrade quality by one when not expired', () => {
+    let shop = new Shop([new Item('foo', 1, 1)]);
+    shop = shop.updateQuality();
     expect(shop.items[0].quality).toBe(0);
-  })
+  });
 
   it('should degrade quality by two when expired', () => {
-    let shop = new Shop([new Item("foo", 1, 10)]);
+    let shop = new Shop([new Item('foo', 1, 10)]);
     shop = shop.updateQuality();
     shop = shop.updateQuality();
     expect(shop.items[0].quality).toBe(7);
-  })
+  });
 });
