443c48966a7a9935b76446e3a367edbbc754a3a3
Dieter Luypaert <hey@moeriki.com>
2022-11-29 11:26:42 +0100

    refactor: rewrite implementation from scratch
    - Phased refactoring would not have been beneficial.
    
    - Renamed `updateQuality` to `update` because we update also `sellIn`.
    
    - I added some domain logic to the `Item` class. I know we were told not
      to touch it, but it just makes a lot of sense.

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,84 +1,108 @@
+const DEFAULT_MAX_QUALITY = 50;
+
+const DEFAULT_QUALITY_DELTA = 1;
+
+const EXPIRED_QUALITY_DELTA = 2;
+
+const LEGENDARY_QUALITY = 80;
+
+const Category = {
+  AGED_QUALITY: 'AGED_QUALITY',
+  LEGENDARY: 'LEGENDARY',
+  TICKETS: 'TICKETS',
+};
+
 class Item {
+  #quality;
+  #sellIn;
+
   constructor(name, sellIn, quality) {
     this.name = name;
-    this.sellIn = sellIn;
     this.quality = quality;
+    this.sellIn = sellIn;
+  }
+
+  get category() {
+    if (this.name === 'Aged Brie') {
+      return Category.AGED_QUALITY;
+    }
+    if (this.name === 'Sulfuras, Hand of Ragnaros') {
+      return Category.LEGENDARY;
+    }
+    if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      return Category.TICKETS;
+    }
+    return null;
+  }
+
+  get isExpired() {
+    return this.sellIn <= 0;
+  }
+
+  get quality() {
+    return this.#quality;
+  }
+
+  set quality(value) {
+    switch (this.category) {
+      case Category.LEGENDARY:
+        this.#quality = LEGENDARY_QUALITY;
+        break;
+      default:
+        this.#quality = Math.max(0, Math.min(DEFAULT_MAX_QUALITY, value));
+    }
+  }
+
+  get sellIn() {
+    return this.#sellIn;
+  }
+
+  set sellIn(value) {
+    switch (this.category) {
+      case Category.LEGENDARY:
+        this.#sellIn = Infinity;
+        break;
+      default:
+        this.#sellIn = value;
+    }
   }
 }
 
 class Shop {
   constructor(items = []) {
-    this.items = items.map(coerceValidItem);
+    this.items = items;
   }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (
-            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
 
-    return new Shop(this.items);
+  update() {
+    return new Shop(
+      this.items.map((item) => {
+        const newItem = new Item(item.name, item.sellIn, item.quality);
+        const defaultQualityDelta = newItem.isExpired
+          ? EXPIRED_QUALITY_DELTA
+          : DEFAULT_QUALITY_DELTA;
+        switch (item.category) {
+          case Category.AGED_QUALITY:
+            newItem.quality += defaultQualityDelta;
+            break;
+          case Category.TICKETS:
+            if (item.sellIn <= 0) {
+              newItem.quality = 0;
+            } else if (item.sellIn <= 5) {
+              newItem.quality += 3;
+            } else if (item.sellIn <= 10) {
+              newItem.quality += 2;
+            } else {
+              newItem.quality += 1;
+            }
+            break;
+          default:
+            newItem.quality -= defaultQualityDelta;
+        }
+        newItem.sellIn -= 1;
+        return newItem;
+      }),
+    );
   }
-}
-
-function coerceValidItem(item) {
-  if (item.quality > 50) {
-    item.quality = 50;
-    console.warn(`${item.name}: quality cannot be greater than 50`);
-  }
-  if (item.name === 'Sulfuras, Hand of Ragnaros') {
-    item.sellIn = Infinity;
-    item.quality = 80;
-  }
-  return item;
 }
 
 module.exports = {
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,96 +1,94 @@
 const { Shop, Item } = require('../src/gilded_rose');
 
-jest.spyOn(console, 'warn').mockImplementation(() => {});
-
 describe('Shop', function () {
   describe('Item', () => {
     it('should degrade sellIn', () => {
       let shop = new Shop([new Item('foo', 1, 0)]);
-      shop = shop.updateQuality();
+      shop = shop.update();
       expect(shop.items[0].sellIn).toBe(0);
     });
 
     it('should degrade quality by one when not expired', () => {
       let shop = new Shop([new Item('foo', 1, 1)]);
-      shop = shop.updateQuality();
+      shop = shop.update();
       expect(shop.items[0].quality).toBe(0);
     });
 
     it('should degrade quality by two when expired', () => {
       let shop = new Shop([new Item('foo', 1, 10)]);
-      shop = shop.updateQuality(); // -1
-      shop = shop.updateQuality(); // -2
+      shop = shop.update(); // -1
+      shop = shop.update(); // -2
       expect(shop.items[0].quality).toBe(7);
     });
 
     it('should not degrade quality under 0', () => {
       let shop1 = new Shop([new Item('foo', 10, 0)]);
-      shop1 = shop1.updateQuality();
-      shop1 = shop1.updateQuality();
+      shop1 = shop1.update();
+      shop1 = shop1.update();
       let shop2 = new Shop([new Item('foo', 0, 0)]);
-      shop2 = shop2.updateQuality();
-      shop2 = shop2.updateQuality();
+      shop2 = shop2.update();
+      shop2 = shop2.update();
       expect(shop1.items[0].quality).toBe(0);
       expect(shop2.items[0].quality).toBe(0);
     });
 
     it('should never have quality over 50', () => {
       const shop = new Shop([new Item('Aged Brie', 1, 55)]);
-      expect(shop.updateQuality().items[0].quality).toBe(50);
+      expect(shop.update().items[0].quality).toBe(50);
     });
 
     it('should never increase quality over 50', () => {
       let shop = new Shop([new Item('Aged Brie', 1, 49)]);
-      shop = shop.updateQuality(); // +1
-      shop = shop.updateQuality(); // +0
+      shop = shop.update(); // +1
+      shop = shop.update(); // +0
       expect(shop.items[0].quality).toBe(50);
     });
   });
 
-  describe('Item: Aged Brie', () => {
+  describe('Category: Aged Quality', () => {
     it('should upgrade over time', () => {
       let shop = new Shop([new Item('Aged Brie', 1, 0)]);
-      shop = shop.updateQuality(); // +1
-      shop = shop.updateQuality(); // +2
-      shop = shop.updateQuality(); // +2
+      shop = shop.update(); // +1
+      shop = shop.update(); // +2
+      shop = shop.update(); // +2
       expect(shop.items[0].quality).toBe(5);
     });
   });
 
-  describe('Item: Sulfuras, Hand of Ragnaros', () => {
+  describe('Category: Legendary', () => {
     it('should never update its quality / sellin', () => {
       let shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 10, 10)]);
       expect(shop.items[0].quality).toBe(80);
       expect(shop.items[0].sellIn).toBe(Infinity);
-      shop = shop.updateQuality();
+      shop = shop.update();
       expect(shop.items[0].quality).toBe(80);
       expect(shop.items[0].sellIn).toBe(Infinity);
     });
   });
 
-  describe('Item: Backstage passes to a TAFKAL80ETC concert', () => {
+  describe('Category: Tickets', () => {
     test('should increase in quality as sellIn approaches', () => {
       let shop = new Shop([
         new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20),
       ]);
-      shop = shop.updateQuality(); // sellIn: 10, quality: 21
-      shop = shop.updateQuality(); // sellIn: 9, quality: 23
+      shop = shop.update(); // sellIn: 10, quality: 21
+      shop = shop.update(); // sellIn: 9, quality: 23
       expect(shop.items[0].sellIn).toBe(9);
       expect(shop.items[0].quality).toBe(23);
-      shop = shop.updateQuality(); // sellIn: 8, quality: 25
-      shop = shop.updateQuality(); // sellIn: 7, quality: 27
-      shop = shop.updateQuality(); // sellIn: 6, quality: 29
-      shop = shop.updateQuality(); // sellIn: 5, quality: 31
-      shop = shop.updateQuality(); // sellIn: 4, quality: 34
+      shop = shop.update(); // sellIn: 8, quality: 25
+      shop = shop.update(); // sellIn: 7, quality: 27
+      shop = shop.update(); // sellIn: 6, quality: 29
+      shop = shop.update(); // sellIn: 5, quality: 31
+      shop = shop.update(); // sellIn: 4, quality: 34
       expect(shop.items[0].sellIn).toBe(4);
       expect(shop.items[0].quality).toBe(34);
-      shop = shop.updateQuality(); // sellIn: 3, quality: 37
-      shop = shop.updateQuality(); // sellIn: 2, quality: 40
-      shop = shop.updateQuality(); // sellIn: 1, quality: 43
+      shop = shop.update(); // sellIn: 3, quality: 37
+      shop = shop.update(); // sellIn: 2, quality: 40
+      shop = shop.update(); // sellIn: 1, quality: 43
       expect(shop.items[0].sellIn).toBe(1);
       expect(shop.items[0].quality).toBe(43);
-      shop = shop.updateQuality(); // sellIn: 0, quality: 47
-      shop = shop.updateQuality(); // sellIn: -1, quality: 0
+      shop = shop.update(); // sellIn: 0, quality: 47
+      shop = shop.update(); // sellIn: -1, quality: 0
       expect(shop.items[0].sellIn).toBe(-1);
       expect(shop.items[0].quality).toBe(0);
     });
